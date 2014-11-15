e740a1018855c4ae0447175528dd6fba027119aa
NamNguyen52 <nam.nguyen52@gmail.com>
2014-11-15 14:11:20 -0800

    fixed overflow bug

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
 $(document).scroll(function(){
   var y = $(this).scrollTop();
 
-  if (y > 800) {
+  if (y > 700) {
     $('nav').fadeIn();
   } else {
     $('nav').fadeOut();
